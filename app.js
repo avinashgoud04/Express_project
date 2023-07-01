65b5267b003abcf179829601c67f92c322a808c8
Avinash <ananthula.avinash@go-mmt.com>
2023-07-01 15:41:22 +0530

    Added more http methods and foratted the code

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-})
+app.use(express.json());
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: "success",
-        results: tours?.length,
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours?.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((item) => item.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  console.log(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({
+        status: 'success',
         data: {
-            tours
-        }
-    })
-})
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
