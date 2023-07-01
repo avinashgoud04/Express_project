ffeb6dcff8618760ebff949af48d00f5b1f4da3b
Avinash <ananthula.avinash@go-mmt.com>
2023-07-01 12:59:11 +0530

    Added get method and sent local response

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 const port = 3000;
@@ -7,6 +8,16 @@ app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 })
 
-app.get('/', (req, res) => {
- res.status(200).json({ message: "Hello from server! ", app: "natours"});
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: tours?.length,
+        data: {
+            tours
+        }
+    })
 })
